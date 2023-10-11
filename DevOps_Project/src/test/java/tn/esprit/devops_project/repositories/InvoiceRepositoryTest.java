8cb40fe8cdd8621ab87eaa2b22ab05c50cef0458
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-10-11 14:09:56 +0100

    init

---
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/repositories/InvoiceRepositoryTest.java
@@ -0,0 +1,17 @@
+package tn.esprit.devops_project.repositories;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvoiceRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/SupplierServiceImplTest.java
@@ -0,0 +1,38 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void retrieveAllSuppliers() {
+    }
+
+    @Test
+    void addSupplier() {
+    }
+
+    @Test
+    void updateSupplier() {
+    }
+
+    @Test
+    void deleteSupplier() {
+    }
+
+    @Test
+    void retrieveSupplier() {
+    }
+}
